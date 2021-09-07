dc71e30cdb7166f9ec092aa4a60d29098b8f1407
jaehoon1022 <hyunsense1022@gmail.com>
2021-09-07 18:14:18 +0900

    @Configuration & Singleton

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -12,22 +12,27 @@ import hello.core.order.OrderServiceImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+//@Bean memberService -> new MemoryMemberRepository()
+//@Bean orderService -> new MemoryMemberRepository()
+// 위처럼 memberService 와 orderService를 호출할때 MemberRepository가 두번호출(객체가 두번생성이된다)
+// 이러한 경우 싱글톤이 깨지는것인지 안깨지는것인지 일단 바로 테스트를 통해서 확인해 본다.
 
 @Configuration
 public class AppConfig {
 
     @Bean
     public MemberService memberService(){
-
+        System.out.println("call AppConfig.memberService");
         return new MemberServiceImpl(memberRepository());
     }
     @Bean
     public MemberRepository memberRepository() {
+        System.out.println("call AppConfig.memberRepository");
         return new MemoryMemberRepository();
     }
     @Bean
     public OrderService orderService(){
-
+        System.out.println("call AppConfig.orderService");
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
     @Bean
