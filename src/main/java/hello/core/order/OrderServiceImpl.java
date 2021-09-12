ddc73d9d54d3ffbd9e4931448e7d2016eb9e7e8e
jaehoon1022 <hyunsense1022@gmail.com>
2021-09-13 07:23:10 +0900

    생성자 주입 선택 이유

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -11,10 +11,11 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class OrderServiceImpl implements OrderService {
-
+    // final : 생성할때 정해지면 바뀌지 않는다는 것이다. final의 장점은 생성자에서만 값을 세팅할수 있다는 것이다.
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
 //    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
+
 
     @Autowired
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
---
+++ src/test/java/hello/core/order/OrderServiceImplTest.java
@@ -0,0 +1,27 @@
+package hello.core.order;
+
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemoryMemberRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class OrderServiceImplTest {
+
+    @Test
+    void createOrder(){
+        //생성자 주입으로 코드를 작성할시 테스트할때 어떠한 인자가 들어가야되는지? 개발자가 바로 인지를 할 수가 있다.
+        MemoryMemberRepository memberRepository = new MemoryMemberRepository();
+        memberRepository.save(new Member(1L, "name", Grade.VIP));
+
+        OrderServiceImpl orderService = new OrderServiceImpl(memberRepository, new FixDiscountPolicy());
+        Order order = orderService.createOrder(1L, "itemA", 10000);
+        assertThat(order.getDiscountPrice()).isEqualTo(1000);
+
+    }
+
+}
